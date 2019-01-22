7e0d2ca9562aa0404ebb7f479ae83b00059d8dec
Faris Habib <faris.habib.7@gmail.com>
2019-01-21 22:58:05 -0800

    Corrected import issue with morgan

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
 const proxy = require('http-proxy-middleware');
+
 const app = express();
 const port = process.env.PORT || 3010;
-
-app.use(cors());
 app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(proxy('/api/videos', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
