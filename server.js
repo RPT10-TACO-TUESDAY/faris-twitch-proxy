4a124e154983c67167025982c22599fa24340107
Faris Habib <faris.habib.7@gmail.com>
2019-01-21 22:53:23 -0800

    Testing proxy deployment

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-
+const proxy = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3010;
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(proxy('/api/videos', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
+app.use(proxy('/api/games', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
+app.use(proxy('/api/users', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
