896e3853d10a5f6d13be0c9ff2d481fef8d6defc
Faris Habib <faris.habib.7@gmail.com>
2019-01-21 22:24:40 -0800

    Test test 12

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3010;
-app.use(express.json());
-app.use(express.urlencoded({extended: true }));
-app.use(express.static(path.join(__dirname, './public')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
